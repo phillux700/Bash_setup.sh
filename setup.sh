d734901025b08382d2e4946a5c9f223984f826b2
sergisergio <ptraon@pm.me>
2020-11-16 16:14:44 +0100

    add setup.sh, setp hostname

---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Fichier de configuration du hostname, du pseudo et du password
+
+# If faut être connecté en tant que root
+if [ $EUID -ne 0 ]; then
+    echo "This script should be run as root." > /dev/stderr
+    exit 1
+fi
+
+# Récupération adresse IP du poste
+ip=`ifconfig ens33 | awk '/inet / {print $2}' | cut -d  ':' -f2`
+#echo $ip
+# Récupération des 2 derniers chiffres de l'IP
+lastNumbers=`echo -n $ip | tail -c 2`
+
+# Hostname
+
+CUR_HOSTNAME=$(cat /etc/hostname)
+
+if [[ $ip == *"100" *]]; then
+	NEW_HOSTNAME="Abeille$lastNumbers"
+elif [[ $ip == *"101" *]]; then
+	NEW_HOSTNAME="Baobab$lastNumbers"
+else
+	echo "Adresse IP inconnue"
+fi
+
+hostname ctl set-hostname $NEW_HOSTNAME
+hostname $NEW_HOSTNAME
+sudo sed -i "s/$CUR_HOSTNAME/$NEW_HOSTNAME/g" /etc/hosts
+sudo sed -i "s/$CUR_HOSTNAME/$NEW_HOSTNAME/g" /etc/hostname
+echo "The new hostname is $NEW_HOSTNAME"
